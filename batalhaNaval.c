072ef705f5e98bed0d286a4cd7d097256951bf40
gustavods783 <gustavods783@gmail.com>
2026-05-26 19:53:55 +0000

    Atualização: Acrescentado navio na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,7 +4,7 @@
 
         int agua [10][10] = {0}; // 0 representa água, 1 representa navio
 
-        int navios = 3; // número de navios a serem colocados
+        int navios = 4; // número de navios a serem colocados
 
         // Colocar os navios no tabuleiro
         agua [0][0] = 1; // navio 1
@@ -16,7 +16,11 @@
         agua [5][2] = 1; // navio 3
         agua [5][3] = 1; // navio 3
         agua [5][4] = 1; // navio 3
-        
+        agua [6][7] = 1; // navio 4
+        agua [7][8] = 1; // navio 4
+        agua [8][9] = 1; // navio 4
+
+
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
